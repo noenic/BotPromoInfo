bbcb47abdc9c4760b740ddb1f3be5e7b3822c49c
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-10-25 15:47:03 +0200

    Ajout d'un fichier de configuration + réarrangement du code, commentaire etc

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 import discord
+from dotenv import load_dotenv
+import os
 
+#Indiquer où est le fichier de config
+load_dotenv(dotenv_path="config")
+
+#Création du client
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
+#Evenements
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -19,4 +25,5 @@ async def on_message(message):
 
         await message.channel.send(f"Ya plus rien le gros lard de Matis Chabanat à tout gobé !")
 
-client.run('MTAzNDQzNzIxOTkxMTQ2NzAzOQ.GMCQIZ.PztRDk_d8QhYeWd9_4pnpHiu4cgum1TtJ1n3cQ')
+#Démarrage du client
+client.run(os.getenv("TOKEN"))
