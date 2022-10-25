9346d6343167e6fefea3d083bb70010c6e34db92
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-10-25 13:26:59 +0200

    Avancement du scripte de récupération du menu

--- menu.py
+++ menu.py
@@ -1,11 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 url = 'https://www.crous-bordeaux.fr/restaurant/resto-u-pierre-bidart/'
-
 r = requests.get(url)
+soup = bs(r.text, 'html.parser')
+element = soup.select_one('div[id="menu-repas"]')
+jours = element.select('h3')
+sElement = element.select('div[class="content clearfix"]')
+tab = []
+for i in range(len(sElement)):
+    tab.append(sElement[i].select_one('ul[class="liste-plats"]'))
+print(tab)
 
-soup = BeautifulSoup(r.text)
-elementMenu = soup.find("inner")
 
-print(elementMenu)
